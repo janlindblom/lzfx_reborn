c54c6a67dd72cf79252ef3c94493ad6c450e8922
Jan Lindblom <janlindblom@fastmail.fm>
2022-07-07 16:21:10 +0300

    Modernising and preparing to port for AVR targets.

--- tests/test.c
+++ tests/test.c
@@ -9,18 +9,15 @@
 
 const char* syntax = "Syntax is 'util in out c|d'\n";
 
-typedef enum {compress, decompress} lzmode_t;
+typedef enum { compress, decompress } lzmode_t;
 typedef unsigned char u8;
-typedef int (*lzfx_fn)(const void* ibuf, unsigned int ilen,
-                             void* obuf, unsigned int *olen);
+typedef int (*lzfx_fn)(const void* ibuf, unsigned int ilen, void* obuf, unsigned int* olen);
 
-int lzf_proxy_comp(const void* ibuf, unsigned int ilen,
-                         void* obuf, unsigned int *olen){
-
+int lzf_proxy_comp(const void* ibuf, unsigned int ilen, void* obuf, unsigned int* olen) {
     unsigned int rc;
 
     rc = lzf_compress(ibuf, ilen, obuf, *olen);
-    if(rc==0){
+    if (rc == 0) {
         return LZFX_ESIZE;
     }
 
@@ -28,16 +25,14 @@ int lzf_proxy_comp(const void* ibuf, unsigned int ilen,
     return 0;
 }
 
-int lzf_proxy_decomp(const void* ibuf, unsigned int ilen,
-                           void* obuf, unsigned int *olen){
-
+int lzf_proxy_decomp(const void* ibuf, unsigned int ilen, void* obuf, unsigned int* olen) {
     unsigned int rc;
 
     rc = lzf_decompress(ibuf, ilen, obuf, *olen);
-    if(rc==0){
-        if(errno==EINVAL){
+    if (rc == 0) {
+        if (errno == EINVAL) {
             return LZFX_ECORRUPT;
-        } else if(errno == E2BIG){
+        } else if (errno == E2BIG) {
             return LZFX_ESIZE;
         } else {
             fprintf(stderr, "unknown lzf fault\n");
@@ -58,12 +53,10 @@ int lzf_proxy_decomp(const void* ibuf, unsigned int ilen,
     Returns:    0   No failure
                 1   Failure
 */
-int test_bounds(const void* ibuf, unsigned int ilen,
-                lzfx_fn compressor, lzfx_fn decompressor){
+int test_bounds(const void* ibuf, unsigned int ilen, lzfx_fn compressor, lzfx_fn decompressor) {
+    unsigned int real_length;
+    u8*          comparison_buffer;
 
-    unsigned int real_length; 
-    u8* comparison_buffer;
-    
     u8* compressed_buffer;
     u8* plaintext_buffer;
 
@@ -74,18 +67,18 @@ int test_bounds(const void* ibuf, unsigned int ilen,
     int rc;
     int frc = 0;
 
-    real_length = ((int)(ilen*1.05) == ilen ? ilen+20 : (int)ilen*1.05) + GUARD_BYTES;    
+    real_length       = ((int)(ilen * 1.05) == ilen ? ilen + 20 : (int)ilen * 1.05) + GUARD_BYTES;
     comparison_buffer = (u8*)malloc(real_length);
     compressed_buffer = (u8*)malloc(real_length);
-    plaintext_buffer = (u8*)malloc(real_length);
+    plaintext_buffer  = (u8*)malloc(real_length);
 
     memset(comparison_buffer, MAGIC_VAL, real_length);
 
-    size_after_compression = real_length-GUARD_BYTES;
+    size_after_compression = real_length - GUARD_BYTES;
 
     /* Determine the actual size of the output data */
     rc = compressor(ibuf, ilen, compressed_buffer, &size_after_compression);
-    if(rc<0){
+    if (rc < 0) {
         fprintf(stderr, "Failed initial compression\n");
         frc = 1;
         goto out;
@@ -98,13 +91,13 @@ int test_bounds(const void* ibuf, unsigned int ilen,
 
     rc = compressor(ibuf, ilen, compressed_buffer, &compressed_length);
 
-    if(memcmp(comparison_buffer, compressed_buffer+size_after_compression, GUARD_BYTES)){
+    if (memcmp(comparison_buffer, compressed_buffer + size_after_compression, GUARD_BYTES)) {
         fprintf(stderr, "Overrun in compressed bytes");
         frc = 1;
         goto out;
     }
 
-    if(rc<0){
+    if (rc < 0) {
         fprintf(stderr, "Failed second compression (code %d)\n", rc);
         frc = 1;
         goto out;
@@ -112,26 +105,25 @@ int test_bounds(const void* ibuf, unsigned int ilen,
 
     plaintext_length = ilen;
 
-    rc = decompressor(compressed_buffer, compressed_length,
-                         plaintext_buffer, &plaintext_length);
+    rc = decompressor(compressed_buffer, compressed_length, plaintext_buffer, &plaintext_length);
 
-    if(rc<0){
+    if (rc < 0) {
         fprintf(stderr, "Failed decompression (code %d)\n", rc);
         frc = 1;
         goto out;
     }
 
-    if(memcmp(comparison_buffer, plaintext_buffer+plaintext_length, GUARD_BYTES)){
+    if (memcmp(comparison_buffer, plaintext_buffer + plaintext_length, GUARD_BYTES)) {
         fprintf(stderr, "Overrun in decompressed bytes\n");
         frc = 1;
     }
 
-    if(memcmp(ibuf, plaintext_buffer, ilen)){
+    if (memcmp(ibuf, plaintext_buffer, ilen)) {
         fprintf(stderr, "Decompressed plaintext does not match\n");
         frc = 1;
     }
 
-    out:
+out:
 
     free(comparison_buffer);
     free(plaintext_buffer);
@@ -141,72 +133,70 @@ int test_bounds(const void* ibuf, unsigned int ilen,
 }
 
 /*  Test round-trip.  1 on failure, 0 on no failure. */
-int test_round(const void* ibuf, unsigned int ilen,
-               lzfx_fn compressor, lzfx_fn decompressor){
-
-    u8* compressed_buffer = NULL;
+int test_round(const void* ibuf, unsigned int ilen, lzfx_fn compressor, lzfx_fn decompressor) {
+    u8*          compressed_buffer = NULL;
     unsigned int compressed_length;
-    u8* plaintext_buffer = NULL;
+    u8*          plaintext_buffer = NULL;
     unsigned int plaintext_length;
 
     int rc;
-    int frc=0;
+    int frc = 0;
 
-    compressed_length = (int)(ilen*1.05) + 16;
+    compressed_length = (int)(ilen * 1.05) + 16;
     compressed_buffer = (u8*)malloc(compressed_length);
 
     plaintext_length = ilen;
     plaintext_buffer = (u8*)malloc(plaintext_length);
 
     rc = compressor(ibuf, ilen, compressed_buffer, &compressed_length);
-    if(rc<0){
+    if (rc < 0) {
         fprintf(stderr, "Failed initial compression (code %d)\n", rc);
         frc = 1;
         goto out;
     }
 
-    rc = decompressor(compressed_buffer, compressed_length,
-                      plaintext_buffer, &plaintext_length);
-    if(rc<0){
+    rc = decompressor(compressed_buffer, compressed_length, plaintext_buffer, &plaintext_length);
+    if (rc < 0) {
         fprintf(stderr, "Failed round-trip decompression (code %d)\n", rc);
         frc = 1;
         goto out;
     }
-    
-    out:
+
+out:
 
     free(compressed_buffer);
     free(plaintext_buffer);
-    
+
     return frc;
 }
 
 /*  Perform test battery on input (plaintext) buffer.  Prints to stdout.
-    
+
     Return is # of failed tests.
 */
-int perform_tests(const void* ibuf, unsigned int ilen, const char* fname){
-
+int perform_tests(const void* ibuf, unsigned int ilen, const char* fname) {
     int nfailed = 0;
-    int rc = 0;
+    int rc      = 0;
 
-#define DO_TEST(exp, msg) \
-  { rc = (exp);     \
-    if(rc){         \
-        nfailed++;  \
-        fprintf(stderr, "\nFail: %s (file %s)\n", (msg), fname); \
-    } else { \
-        fprintf(stdout, "."); \
-    } \
-    rc = 0; }
+#define DO_TEST(exp, msg)                                            \
+    {                                                                \
+        rc = (exp);                                                  \
+        if (rc) {                                                    \
+            nfailed++;                                               \
+            fprintf(stderr, "\nFail: %s (file %s)\n", (msg), fname); \
+        } else {                                                     \
+            fprintf(stdout, ".");                                    \
+        }                                                            \
+        rc = 0;                                                      \
+    }
 
     DO_TEST(test_round(ibuf, ilen, lzf_proxy_comp, lzf_proxy_decomp), "LZF round trip");
-    DO_TEST(test_round(ibuf, ilen, lzfx_compress, lzfx_decompress),   "LZFX round trip");
+    DO_TEST(test_round(ibuf, ilen, lzfx_compress, lzfx_decompress), "LZFX round trip");
 
     DO_TEST(test_round(ibuf, ilen, lzfx_compress, lzf_proxy_decomp), "LZFX comp -> LZF decomp");
     DO_TEST(test_round(ibuf, ilen, lzf_proxy_comp, lzfx_decompress), "LZF comp -> LZFX decomp");
- 
-    DO_TEST(test_bounds(ibuf, ilen, lzfx_compress, lzfx_decompress),   "LZFX overrun check");
+
+    DO_TEST(test_bounds(ibuf, ilen, lzfx_compress, lzfx_decompress), "LZFX overrun check");
     DO_TEST(test_bounds(ibuf, ilen, lzf_proxy_comp, lzf_proxy_decomp), "LZF overrun check");
 
     fprintf(stdout, "\n");
@@ -222,50 +212,48 @@ int perform_tests(const void* ibuf, unsigned int ilen, const char* fname){
                  1  I/O error
                  2  Test failure
 */
-int main(int argc, char* argv[]){
-
-    int fd;
-    u8 *ibuf = NULL;
+int main(int argc, char* argv[]) {
+    int          fd;
+    u8*          ibuf = NULL;
     unsigned int ilen = 0;
-    
+
     ssize_t rc;
     ssize_t amt_read = 0;
-    int nblocks = 0;
+    int     nblocks  = 0;
 
     int argidx;
     int nfailed = 0;
 
-    if(argc<2){
+    if (argc < 2) {
         fprintf(stderr, "Syntax is \"test file1 file2 ... fileN\"\n");
         return 2;
     }
 
-    for(argidx=1; argidx<argc; argidx++){
-
+    for (argidx = 1; argidx < argc; argidx++) {
         amt_read = 0;
         free(ibuf);
-        ibuf = NULL;
-        ilen = 0;
+        ibuf    = NULL;
+        ilen    = 0;
         nblocks = 0;
-        rc = 0;
+        rc      = 0;
 
-        if((fd = open(argv[argidx], O_RDONLY)) < 0){
+        if ((fd = open(argv[argidx], O_RDONLY)) < 0) {
             fprintf(stderr, "Can't open input file \"%s\".\n", argv[argidx]);
             return 2;
         }
         do {
-            if((ilen-amt_read) < BLOCKSIZE){
-                ibuf = realloc(ibuf, ilen+BLOCKSIZE);
+            if ((ilen - amt_read) < BLOCKSIZE) {
+                ibuf = realloc(ibuf, ilen + BLOCKSIZE);
                 ilen += BLOCKSIZE;
             }
             rc = read(fd, ibuf, BLOCKSIZE);
-            if(rc<0){
+            if (rc < 0) {
                 fprintf(stderr, "Read error\n");
                 return 2;
             }
             amt_read += rc;
-        } while(rc > 0);
-        
+        } while (rc > 0);
+
         ilen = amt_read;
 
         close(fd);
@@ -273,7 +261,7 @@ int main(int argc, char* argv[]){
         nfailed += perform_tests(ibuf, ilen, argv[argidx]);
     }
 
-    if(nfailed){
+    if (nfailed) {
         fprintf(stdout, "%d test%s failed\n", nfailed, nfailed > 1 ? "s" : "");
     } else {
         fprintf(stdout, "All tests passed\n");
@@ -281,10 +269,3 @@ int main(int argc, char* argv[]){
 
     return !!nfailed;
 }
-
-
-
-
-
-
-
